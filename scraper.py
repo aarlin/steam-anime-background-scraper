0d538e842c199a6e6d14a6879525146dc910d831
fompei <linaaron1995@gmail.com>
2017-09-15 12:08:56 -0400

    Found anime games for one page

--- scraper.py
+++ scraper.py
@@ -1,5 +1,24 @@
+# BeautifulSoup implementation
+
 from urllib import urlopen
 from bs4 import BeautifulSoup
-html = urlopen("http://www.pythonscraping.com/pages/page1.html")
-bsObj = BeautifulSoup(html.read())
-print(bsObj.h1)
+import re
+
+def pagescraper(searchpage):
+    html = urlopen(searchpage)
+    bsObj = BeautifulSoup(html.read())
+    for link in bsObj.findAll("a", href = re.compile("^(http://store.steampowered.com/app/.*)$")):
+        if 'href' in link.attrs:
+            print(link.attrs['href'])
+
+def totalpages(searchpage):
+    html = urlopen(searchpage)
+
+
+# <a href="http://store.steampowered.com/appa/ + ^*?  + "
+
+# need to go through all pages as well
+
+pagescraper("http://store.steampowered.com/search/?tags=4085&category2=29")
+
+#<div class="search_pagination_right">
